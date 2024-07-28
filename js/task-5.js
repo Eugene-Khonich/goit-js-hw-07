98ec47b5c4d06f26732a44028ff1fdcb0af6b472
Eugene-Khonich <ehonich@gmail.com>
2024-07-28 22:50:58 +0300

    123123

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,10 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const changeColorBtn = document.querySelector('.change-color');
+const body = document.querySelector('body');
+const colorOutput = document.querySelector('.color');
+
+changeColorBtn.addEventListener('click', getRandomHexColor);
+
+const onBtnClick = 
