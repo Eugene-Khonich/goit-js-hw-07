73f7b2e888a718a780ad0a7694d6c0ad4f5340fe
Eugene-Khonich <ehonich@gmail.com>
2024-07-28 22:02:46 +0300

    add task four

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,13 @@
+const form = document.querySelector('.login-form');
 
+const onFormSubmit = event => {
+  event.preventDefault();
+  const formData = {
+    email: form.elements.email.value.trim(),
+    password: form.elements.password.value.trim(),
+  };
+  form.reset();
+  console.log(formData);
+};
+
+form.addEventListener('submit', onFormSubmit);
