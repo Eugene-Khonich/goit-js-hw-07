ff916216d1de6fc8e4a0a158f6be1292166ea6a3
Eugene-Khonich <ehonich@gmail.com>
2024-07-30 22:08:02 +0300

    add task six

--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,41 @@
+const controls = document.querySelector('#controls');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxes = document.querySelector('#boxes');
+const input = document.querySelector('input');
+
+createBtn.addEventListener('click', createItems);
+destroyBtn.addEventListener('click', destroyBoxes);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+function createBoxes(amount) {
+  amount = Number(amount);
+  destroyBoxes();
+  let size = 30;
+  const boxesArr = [];
+  for (let i = 0; i < amount; i++) {
+    boxesArr.push(
+      `<div style="height:${size}px;width:${size}px;background-color:${getRandomHexColor()}"></div>`
+    );
+    size += 10;
+  }
+  boxes.insertAdjacentHTML('beforeend', boxesArr.join(''));
+  input.value = null;
+}
+
+function createItems() {
+  if (input.value > 0 && input.value <= 100) {
+    createBoxes(input.value);
+  } else {
+    alert('Введіть значення від 1 до 100');
+  }
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = '';
 }
